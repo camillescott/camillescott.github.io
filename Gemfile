662f5024630ffe16bb093c2b7f11cb67368784b2
Camille Scott <camille.scott.w@gmail.com>
2022-03-24 14:58:40 -0700

    add proper flake

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
 gem 'github-pages', group: :jekyll_plugins
+gem "jekyll", "~> 4.2"
+gem "jekyll-sitemap", "~> 1.4"
--- flake.nix
+++ flake.nix
@@ -1,14 +1,58 @@
 {
-  description = "camillescott.github.io";
+  description = "flake for camillescott.github.io";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      ws-gems = pkgs.bundlerEnv {
+        name = "ws-gems";
+        inherit (pkgs.ruby);
+        gemdir = ./.;
+      };
+      ws = pkgs.stdenv.mkDerivation {
+        name = "ws";
+        src = self;
+        buildPhase = ''
+          bundle exec jekyll build
+        '';
+        installPhase = ''
+          cp -r _site/. $out
+        '';
+        buildInputs = [
+          ws-gems
+          pkgs.ruby
+        ];
+      };
+      ws-host = pkgs.writeShellScriptBin "ws-host" ''
+        cd ${ws}
+        ${pkgs.ruby}/bin/ruby -run -e httpd -- .
+      '';
+    in
+    {
+      packages = flake-utils.lib.flattenTree {
+        inherit ws;
+      };
+      defaultPackage = self.packages.${system}.ws;
+      apps = {
+        ws-host = flake-utils.lib.mkApp { 
+          drv = ws-host;
+        };
+      };
+      defaultApp = self.apps.${system}.ws-host;
+      devShell = pkgs.mkShell {
+        buildInputs = ws.buildInputs ++ [
+          pkgs.bundix
+        ];
+      };
+    }
+  );
 }
+
